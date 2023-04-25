36ce27728117e248218fc5451fe53a2a880bdb4d
TJ <thomaskeith17@gmail.com>
2023-04-25 16:55:09 -0600

    finished prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Hello welcome to my random number guess game!");
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(0, 10);
+        int guess;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            string answer = Console.ReadLine();
+            guess = int.Parse(answer);
+
+            if (guess < number)
+            {
+                Console.WriteLine("higher");
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("lower");
+            }
+        } while (guess != number);
+
+        Console.WriteLine("Congratulations! You guessed the number!");
     }
 }
