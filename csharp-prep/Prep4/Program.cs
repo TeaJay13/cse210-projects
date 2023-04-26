bde80c0c3193da3ec5f364c4aba3f9da4f064973
TJ <thomaskeith17@gmail.com>
2023-04-26 02:25:14 -0600

    finished prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Hello welcome to number list creator");
+
+        List<int> numbers = new List<int>();
+        int number = -1;
+
+        while(number != 0)
+        {
+            Console.WriteLine("Enter your number (enter 0 when finished) enter here ");
+            string input = Console.ReadLine();
+            number = int.Parse(input);
+            numbers.Add(number);
+        }
+
+        int sum = 0;
+
+        foreach(int num in numbers)
+        {
+            sum += num;
+        }
+        Console.WriteLine($"The sum is {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+        foreach(int num in numbers)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+        Console.WriteLine($"The max is: {max}");
     }
 }
